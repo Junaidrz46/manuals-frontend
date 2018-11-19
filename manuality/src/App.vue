e3fa5cc2c44d700f52cf9032144709e2db3b3421
nedzadhafizovic <nedzad.hafizovic10@gmail.com>
2018-11-19 12:03:13 +0100

    Login page form and design

---
+++ manuality/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body
+{
+margin:0;
+padding:0;
+background-size:cover;
+font-family:sans-serif;
+background-color: lightgray;
+}
+h1
+{
+	margin:0;
+	text-align:center;
+	color:#F08080;
+}
+
+</style>
---
+++ manuality/src/components/Login.vue
@@ -0,0 +1,112 @@
+<template>
+    <div id="login">
+        <div class="loginBox">
+            <form>
+                <h2>Login</h2>
+                <p>Email</p>
+                <input type="text" name="" placeholder="E-mail">
+                <p>Password</p>
+                <input type="password" name="" placeholder="*****">
+                <input type="Submit" name="" value="Sign In">
+                <a href="#">Forgot your password?</a>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style>
+.loginBox
+{
+	position:absolute;
+	top:50%;
+	left:50%;
+	transform:translate(-50%,-50%);
+	width:350px;
+	height:420px;
+	padding:80px 40px;
+	box-sizing:cover;
+	background:rgba(0,0,0.2);
+    background-size:border-box;
+    opacity: 0.7;
+    margin-top: 70px;
+    margin-left: 600px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	
+}
+.user
+{
+	width:100px;
+	height:100px;
+	border-radius:50%;
+	overflow: hidden;
+	position:absolute;
+	top:calc(-100px/2);
+    left:calc(50%-50px);
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+h2
+{
+	margin:0;
+	padding:0 0 20px;
+	color:#FFFFFF;
+    text-align:center;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.loginBox p 
+{
+	margin:0;
+	padding:0 0 20px;
+	color:#FFFFFF;
+    font-weight:bold;
+    text-align: left;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.loginBox input
+{
+	width:100%;
+    margin-bottom:20px;
+    
+}
+.loginBox input[type="text"], .loginBox input[type="password"]
+{
+	border:none;
+	border-bottom:1px solid #fff;
+	background:transparent;
+	outline:none;
+	height:40px;
+	color:#fff;
+	font-size:16px;
+}
+::placeholder
+{
+    color:rgba(255,255,255,0.5)
+    
+    
+}
+.loginBox input[type="Submit"]
+{
+    margin-top: 45px;
+	border:none;
+	outline:none;
+	height:40px;
+	color:#fff;
+	font-size:16px;
+	background:#708090;
+	cursor:pointer;
+    border-radius:20px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.loginBox input[type="Submit"]:hover
+{
+	background:#FFFFFF;
+	color:#262626;
+}
+.loginBox a
+{
+     color:#fff;
+font-size:14px;
+font-weight:bold;
+text-decoration:none;	 
+}
+</style>
+
