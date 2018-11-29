fb668619aaa6e999dcf4d999c380db851a68797c
meysam-mfard <mmohagheghi@gmail.com>
2018-11-29 17:04:13 +0100

    sidebar update

--- manuality/src/components/Sidebar.vue
+++ manuality/src/components/Sidebar.vue
@@ -1,17 +1,17 @@
 <template>
-    <!-- 
+    
 
-    <b-container fluid>
-
-      <b-row align-v="center">
-
-
-        <b-col lg=3> -->
           <div role="tablist">
               
+              <b-card style="border: 0px; border-radius: 0px; margin-bottom: -1px!important;" no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" style="border-bottom: 0px;" role="tab">
+                  <b-btn block href="#"  class="title-btn" style="vertext-bottom;" size="lg">Manual Directory</b-btn>
+                </b-card-header>
+              </b-card>
+
               <b-card v-bind:key="category_entry" v-for="category_entry in this.categories" style="border: 0px; border-radius: 0px; margin-bottom: -1px!important;" no-body class="mb-1">
                 <b-card-header header-tag="header" class="p-1" style="border-bottom: 0px;" role="tab">
-                  <b-btn block href="#" v-b-toggle="category_entry"  class="cat-btn" size="lg">{{category_entry.category}}</b-btn>
+                  <b-btn block href="#" v-b-toggle="category_entry"  class="cat-btn" size="lg">{{category_entry.name}}</b-btn>
                 </b-card-header>
                 <b-collapse :id="category_entry" accordion="my-accordion" role="tabpanel">
                   <b-card-body >
@@ -19,7 +19,7 @@
                             <div role="tablist">
                                 <b-card v-bind:key="brand_entry" v-for="brand_entry in category_entry.brands" style="border: 0px!important; border-radius: 0px; margin-bottom: -1px!important; " no-body class="mb-1">
                                   <b-card-header header-tag="header" class="p-1" role="tab" style="border-bottom: 0px; background-color: rgba(0,0,0,0.0)">
-                                    <b-btn block href="#" v-b-toggle="brand_entry" class="prod-btn" size="md">{{brand_entry.brand}}</b-btn>
+                                    <b-btn block href="#" v-b-toggle="brand_entry" class="brand-btn" size="md">{{brand_entry.name}}</b-btn>
                                   </b-card-header>
                                   
                                       <b-collapse :id="brand_entry" accordion="my-accordion1" class="mt-2">
@@ -27,9 +27,9 @@
                                           <b-btn
                                             v-bind:key="product_entry"
                                             block href="#"
-                                            variant="outline-success"
+                                            class="prod-btn"
                                             size="sm"
-                                            v-for="product_entry in brand_entry.products"> {{product_entry.product}} </b-btn>
+                                            v-for="product_entry in brand_entry.products"> {{product_entry.name}} </b-btn>
                                         </b-card>
                                       </b-collapse>
                                 </b-card>
@@ -40,10 +40,6 @@
               </b-card>
               
           </div>
-        <!-- </b-col>
-
-      </b-row>
-    </b-container> -->
 
 
 
@@ -57,8 +53,8 @@ export default {
   name: 'Sidebar',
   data () {
     return {
-      // categories: this.getAllCategories()
-      categories: [
+      categories: []
+      /* categories: [
         {
           category: 'cat1',
           brands :[
@@ -103,34 +99,80 @@ export default {
           ]
         }
       
-      ] 
+      ]  */
     }
-  }
-  // ,
-  // methods: {
-  //   populateSidebar: function() {
-  //     getAllCategories().then(response => {
-  //           response.forEach(element => {
-  //               this.categories.push(element)
-  //           });
-  //           console.log(this.cat)
-  //         }
-  //     )
-  //   }
-  // } 
+  },
+  beforeMount: function() {
+            getAllCategories().then(response => {
+                
+                response.forEach(element => {
+                    this.categories.push(element)
+                    
+                });
+
+
+                console.log(response.element.Object)
+            })
+            //localStorage.setItem("latestAddedProduct", '5bfd83619110f2297fc7de38');
+        },
+
+  methods: {
+    populateSidebar: function() {
+      getAllCategories().then(response => {
+            response.forEach(element => {
+                this.categories.push(element)
+            });
+            console.log(this.cat)
+          }
+      )
+    }
+  } 
 }
 </script>
 
 <style>
+
+.title-btn
+{
+  border-color: rgb(255, 255, 255);
+  background-color: rgb(255, 255, 255);
+  font-weight: bold;
+  font-size: 25px;
+  color: black;
+  height: 70px;
+  padding-top:15px;
+  margin-left: 0px !important;
+}
+
 .cat-btn
 {
   border-color: rgb(52,58,64);
   background-color: rgb(52,58,64);
+  font-weight: bold;
+  margin-left: 0px !important;
 }
 
-.‌prod-btn
+.brand-btn
 {
-  border-color: rgb(97,135,172);
-  background-color: rgb(1, 69, 79);
+  border-color: rgb(108,117,125);
+  background-color: rgb(108,117,125);
+  font-weight: bold;
+  color: rgb(255, 255, 255);
+  margin-left: 0px !important;
+}
+
+.prod-btn
+{
+  border-color: rgb(196, 205, 214);
+  background-color: rgb(196, 205, 214);
+  font-weight: bold;
+  color: rgb(52,58,64);
+  margin-left: 0px !important;
+}
+
+.card
+{
+  background-color:transparent; 
+  margin-left: 0px !important;
 }
 </style>
